eb1fa591cc6a28dd84d0977917ca50d4498efd44
Tmc <494521535@qq.com>
2019-10-17 17:25:18 +0800

    初步的拼接

---
+++ ModelAssembly/Assets/AssambleObject.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AssambleObject : MonoBehaviour
+{
+
+    private Transform mMark;
+
+    private Transform tMark;
+
+    public Transform TAssambleObject;
+
+    // Use this for initialization
+    void Start()
+    {
+        mMark = this.transform.Find("LocationMark");
+        if (mMark == null)
+        {
+            Debug.LogError("本地对象未找到位置标志");
+        }
+
+        tMark = TAssambleObject.Find("LocationMark");
+        if (tMark == null)
+        {
+            Debug.LogError("目标对象未找到位置标志");
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            MoveToTarget();
+        }
+    }
+
+
+    int j = 0;
+    private void MoveToTarget()
+    {
+
+        Vector3 RotateAix = Vector3.Cross(mMark.transform.forward, tMark.transform.forward);
+        float angle = Vector3.Angle(mMark.transform.forward, tMark.transform.forward);
+        mMark.transform.parent.Rotate(RotateAix, angle, Space.World);
+        while (true)
+        {
+            //第一步
+            float Angle = Vector3.Angle(tMark.transform.up, mMark.transform.up);
+            mMark.transform.parent.Rotate(tMark.transform.forward, Angle, Space.World);
+            //第二步
+            Vector3 moveVector = tMark.transform.position - mMark.transform.position;
+            mMark.transform.parent.transform.Translate(moveVector, Space.World);
+            j++;
+            if (Angle == 0 && moveVector == Vector3.zero)
+            {
+                Debug.Log("进行了多少次数：" + j);//发现一次不是能达到目标，所以重复几次
+                break;
+            }
+        }
+    }
+}
