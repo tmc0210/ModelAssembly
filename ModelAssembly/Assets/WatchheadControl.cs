2f506e6b8848ba2addc36836d6f5d7c358945425
Tmc <494521535@qq.com>
2019-10-27 17:17:43 +0800

    移植到具体模型上

---
+++ ModelAssembly/Assets/WatchheadControl.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WatchheadControl : MonoBehaviour
+{
+    public List<GameObject> WatchHeadModel = new List<GameObject>();
+    private int index;
+
+    void Start()
+    {
+        index = 0;
+        WatchHeadShow();
+    }
+
+    private void WatchHeadShow()
+    {
+        if (index < WatchHeadModel.Capacity)
+        {
+            WatchHeadModel[index].SetActive(true);
+            index++;
+            Invoke("WatchHeadShow", 0.2f);
+        }
+        else
+        {
+            Debug.Log("hide");
+            index = 0;
+            Invoke("WatchHeadHide", 2f);
+        }
+    }
+
+    private void WatchHeadHide()
+    {
+        for (int i = 0; i < WatchHeadModel.Capacity; i++)
+        {
+            WatchHeadModel[i].SetActive(false);
+        }
+        Invoke("WatchHeadShow", 0.5f);
+    }
+}
